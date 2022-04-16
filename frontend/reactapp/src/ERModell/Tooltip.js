9d1fe687214e3226cad15c23920944865e9abc8b
simon.ruttmann <simon.ruttmann@freenet.de>
2022-04-16 18:10:30 +0200

    refactor(frontend/Tooltip): Refactoring of Tooltip

--- frontend/reactapp/src/ERModell/Tooltip.js
+++ frontend/reactapp/src/ERModell/Tooltip.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import {erType_TooltipDescription, erType_TooltipTitle, erType_TooltipImage} from "./ErTypesEnum";
+import {ERTYPE} from "./ErTypesEnum";
 
 const ToolTip = ({erType}) => {
     return (
         <div>
-            {erType_TooltipTitle[erType]}
-            {erType_TooltipDescription[erType]}
-            {erType_TooltipImage[erType]}
+            {ERTYPE[erType].toolTipTitle}
+            {ERTYPE[erType].toolTipDescription}
+            {ERTYPE[erType].toolTipImage}
         </div>
     )
 }
