ad7fe85ec3c1fb5cdf9bfa249b5719966f341ca8
simon.ruttmann <simon.ruttmann@freenet.de>
2022-04-16 18:10:06 +0200

    refactor(frontend/DragBarImageManager): Refactoring of DragBarImageManager

--- frontend/reactapp/src/ERModell/DragBarImageManager.js
+++ frontend/reactapp/src/ERModell/DragBarImageManager.js
@@ -1,12 +1,12 @@
 import React from "react";
-import {erType_DraggableIcon} from "./ErTypesEnum";
+import {ERTYPE} from "./ErTypesEnum";
 import ReactTooltip from "react-tooltip";
 import ToolTip from "./Tooltip";
 
 const DragBarManager = ({erTypes}) => {
 
     function resolveErType(erType){
-        return erType_DraggableIcon[erType];
+        return ERTYPE[erType].icon;
     }
 
     function setDataTransfer(dragEvent, erType){
@@ -31,34 +31,9 @@ const DragBarManager = ({erTypes}) => {
                     </div>
                 ))}
 
-                {/*
-            <ReactTooltip id="NormalAttribute" effect="solid" place={"right"}/>
-            <ReactTooltip id="IdentifyingAttribute" effect="solid" place={"right"}/>
-            <ReactTooltip id="StrongEntity" effect="solid" place={"right"}><p>Hallo Starke ENtittäte</p></ReactTooltip>
-*/}
                 </React.Fragment>
 
     )
 }
 
-// Entities
-// Strong Entity        Weak Entity
-
-// Relations
-// Strong Relation      Weak Relation
-
-// Attributes
-// Attribute            AttributeIdentifying
-// MultivaluedAttribute WeakAttribute
-
-// IsA
-// IsA Structure
-
-
-// display: inline-block
-// width: 34px; height: 32px;
-// padding: 1px;
-// overflow hidden
-
-//
 export default DragBarManager;
