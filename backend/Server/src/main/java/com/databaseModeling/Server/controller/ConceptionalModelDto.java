afb19d8de736f47b2904b496038c9528e19ed212
simon.ruttmann <simon.ruttmann@freenet.de>
2022-05-05 22:44:45 +0200

    feature(backend/model): Add DTO for conceptional model and controller

---
+++ backend/Server/src/main/java/com/databaseModeling/Server/controller/ConceptionalModelDto.java
@@ -0,0 +1,76 @@
+package com.databaseModeling.Server.controller;
+
+import com.databaseModeling.Server.model.ErType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ *
+ * First paragraph.
+ * <p><ul>
+ * <li>the first item
+ * <li>the second item
+ * <li>the third item
+ * </ul><p>
+ * Second paragraph.
+ *
+ * <p>abc</p>This class is representing the object received by the client
+ * @see Controller#convertToRelational(ConceptionalModelDto)
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ConceptionalModelDto {
+
+    private String projectVersion;
+    private String projectName;
+    private String type;
+    private DrawBoardContent drawBoardContent;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class DrawBoardContent{
+
+        private List<Element> elements;
+        private List<Connection> connections;
+
+
+        @Data
+        @NoArgsConstructor
+        @AllArgsConstructor
+        public static class Element{
+            private String id;
+            private String displayName;
+            //private Boolean isHighlighted;
+            //private Boolean isSelected;
+            private String xPos;
+            private String yPos;
+            private String width;
+            private String height;
+            private String objectType;  //Enum DrawBoardElement ...
+            private ErType erType;      //Enum IdentifyingAttribute ...
+        }
+
+        @Data
+        @NoArgsConstructor
+        @AllArgsConstructor
+        public static class Connection{
+
+            private String id;
+            private String start;
+            private String end;
+            private String min;
+            private String max;
+            private String objectType;
+            private Boolean withArrow;
+
+        }
+    }
+
+
+}
+
--- backend/Server/src/main/java/com/databaseModeling/Server/controller/Controller.java
+++ backend/Server/src/main/java/com/databaseModeling/Server/controller/Controller.java
@@ -1,8 +1,16 @@
 package com.databaseModeling.Server.controller;
 
+import com.databaseModeling.Server.model.ElementMetaInformation;
+import com.databaseModeling.Server.model.Graph;
+import com.databaseModeling.Server.model.Node;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 @RestController
 public class Controller {
@@ -19,6 +27,95 @@ public class Controller {
         return m;
     }
 
+    @PostMapping("/convert/relational")
+    public ConceptionalModelDto convertToRelational(
+            @RequestBody ConceptionalModelDto type)
+    {
+        //Idee für später Node Abstrakte klasse implementiert INode, Edge Abstrakte klasse implementiert IEdge
+        //Graph <N,E> mit extends Inode extends IEdge
+        //Graph <Element, Association>
+        System.out.println(type.getType());
+        System.out.println(type.getProjectName());
+        System.out.println(type.getProjectVersion());
+
+        ConceptionalModelDto conceptionalModelDto = new ConceptionalModelDto();
+        conceptionalModelDto.setProjectName("projectNameValue");
+        conceptionalModelDto.setType("typeValue");
+        conceptionalModelDto.setProjectVersion("1.1.1.1");
+
+        ConceptionalModelDto.DrawBoardContent drawBoardContent = new ConceptionalModelDto.DrawBoardContent();
+        ConceptionalModelDto.DrawBoardContent.Element element0 = new ConceptionalModelDto.DrawBoardContent.Element();
+        ConceptionalModelDto.DrawBoardContent.Element element1 = new ConceptionalModelDto.DrawBoardContent.Element();
+        ConceptionalModelDto.DrawBoardContent.Element element2 = new ConceptionalModelDto.DrawBoardContent.Element();
+        ConceptionalModelDto.DrawBoardContent.Connection connection0 = new ConceptionalModelDto.DrawBoardContent.Connection();
+        ConceptionalModelDto.DrawBoardContent.Connection connection1 = new ConceptionalModelDto.DrawBoardContent.Connection();
+        ConceptionalModelDto.DrawBoardContent.Connection connection2 = new ConceptionalModelDto.DrawBoardContent.Connection();
+
+        element0.setId("Id0");
+        element0.setDisplayName("Element0");
+        element0.setXPos("500");
+        element0.setYPos("500");
+        element0.setHeight("100");
+        element0.setWidth("200");
+
+        element1.setId("Id1");
+        element1.setDisplayName("Element1");
+        element1.setXPos("500");
+        element1.setYPos("500");
+        element1.setHeight("100");
+        element1.setWidth("200");
+
+        element2.setId("Id2");
+        element2.setDisplayName("Element2");
+        element2.setXPos("500");
+        element2.setYPos("500");
+        element2.setHeight("100");
+        element2.setWidth("200");
+
+        connection0.setId("IdCon0");
+        connection0.setStart("StartId");
+        connection0.setEnd("EndId");
+        connection0.setMin("1");
+        connection0.setMax("N");
+
+        connection1.setId("IdCon1");
+        connection1.setStart("StartId");
+        connection1.setEnd("EndId");
+        connection1.setMin("1");
+        connection1.setMax("N");
+
+        connection2.setId("IdCon2");
+        connection2.setStart("StartId");
+        connection2.setEnd("EndId");
+        connection2.setMin("1");
+        connection2.setMax("N");
+
+        var elements = new ArrayList<ConceptionalModelDto.DrawBoardContent.Element>();
+        elements.add(element0);
+        elements.add(element1);
+        elements.add(element2);
+
+        var connections = new ArrayList<ConceptionalModelDto.DrawBoardContent.Connection>();
+        connections.add(connection0);
+        connections.add(connection1);
+        connections.add(connection2);
+
+        drawBoardContent.setElements(elements);
+        drawBoardContent.setConnections(connections);
+
+        conceptionalModelDto.setDrawBoardContent(drawBoardContent);
+
+        Graph graph = new Graph();
+
+        var nodeElements = drawBoardContent.getElements().stream().filter(element -> element.getErType().isNode).collect(Collectors.toList());
+        nodeElements.forEach(nodeElement -> {
+            ElementMetaInformation elementMetaInformation = new ElementMetaInformation(); //TODO add meta information
+            graph.addNode(new Node(nodeElement.getId(), nodeElement.getErType(), elementMetaInformation));
+        });
+
+
+        return conceptionalModelDto;
+    }
 
 }
 
