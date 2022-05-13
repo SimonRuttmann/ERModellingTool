04a99b1a44783e885c1584260aba54aa4f7f7650
simon.ruttmann <simon.ruttmann@freenet.de>
2022-05-13 14:11:33 +0200

    feature(backend/model/ErType): Added display name of er types

--- backend/Server/src/main/java/com/databaseModeling/Server/model/ErType.java
+++ backend/Server/src/main/java/com/databaseModeling/Server/model/ErType.java
@@ -1,20 +1,21 @@
 package com.databaseModeling.Server.model;
 
 public enum ErType {
-    StrongEntity(true),
-    WeakEntity(true),
-    StrongRelation(true),
-    IdentifyingRelation(true),
-    IsAStructure(true),
-    IdentifyingAttribute(false),
-    WeakIdentifyingAttribute(false),
-    NormalAttribute(false),
+    StrongEntity(true, "Strong Entity"),
+    WeakEntity(true, "Weak Entity"),
+    StrongRelation(true, "Strong Relation"),
+    IdentifyingRelation(true, "Identifying Relation"),
+    IsAStructure(true, "Is A Structure"),
+    IdentifyingAttribute(false, "Identifying Attribute"),
+    WeakIdentifyingAttribute(false, "Weak identifying Attribute"),
+    NormalAttribute(false, "Normal Attribute"),
    // CompoundAttribute(false),
-    MultivaluedAttribute(false);
+    MultivaluedAttribute(false, "Multivalued Attribute");
 
     public final boolean isNode;
-
-    private ErType (boolean isNode){
+    public final String displayName;
+    ErType (boolean isNode, String displayName){
         this.isNode = isNode;
+        this.displayName = displayName;
     }
 }
