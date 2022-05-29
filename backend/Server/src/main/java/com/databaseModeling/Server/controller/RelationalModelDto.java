75e6017d8841545b1af16512a2d2b8bf472a896a
simon.ruttmann <simon.ruttmann@freenet.de>
2022-05-29 23:10:15 +0200

    feature(backend/RelationalModelDto): Adjustment of RelationalModelDto

--- backend/Server/src/main/java/com/databaseModeling/Server/controller/RelationalModelDto.java
+++ backend/Server/src/main/java/com/databaseModeling/Server/controller/RelationalModelDto.java
@@ -34,6 +34,8 @@ public class RelationalModelDto{
             private String displayName;
             private double x;
             private double y;
+            private double width = 0.0;
+            private double height = 0.0;
             private List<ColumnDTO> columns = new ArrayList<>();
 
             @Data
