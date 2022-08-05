beb4b3d4f174af1c3f211816815a481b5b93b8ca
simon.ruttmann <simon.ruttmann@freenet.de>
2022-08-05 23:30:06 +0200

    feature(frontend/indexJs): Provide redux store

--- frontend/reactapp/src/index.js
+++ frontend/reactapp/src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import store from "./store/Store";
 
 ReactDOM.render(
-  <App/>, document.getElementById('root')
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
