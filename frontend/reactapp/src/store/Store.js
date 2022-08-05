b99e49734b086575aaf0a5175fd08b9f611d1c3b
simon.ruttmann <simon.ruttmann@freenet.de>
2022-08-05 21:44:03 +0200

    feature(frontend/store): Added redux store

---
+++ frontend/reactapp/src/store/Store.js
@@ -0,0 +1,13 @@
+import { configureStore} from "@reduxjs/toolkit";
+import relationalContentSlice from "./RelationalContentSlice";
+import erContentSlice from "./ErContentSlice";
+
+
+export const store = configureStore({
+    reducer: {
+        erContent: erContentSlice,
+        relationalContent: relationalContentSlice()
+    }
+})
+
+export default store;
