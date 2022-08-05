0c30cd2c3389c002d29d7b776fedd16dc9defc4a
simon.ruttmann <simon.ruttmann@freenet.de>
2022-08-05 21:44:15 +0200

    feature(frontend/store): Added relational content slice

---
+++ frontend/reactapp/src/store/RelationalContentSlice.js
@@ -0,0 +1,75 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const relationalContentSlice = createSlice({
+    name: 'relationalContent',
+    initialState: {drawBoardElements: [], connections: []},
+    reducers: {
+        /**
+         *
+         * @param state
+         * @param action
+         */
+        ImportContent: (state, action) => {
+            let drawBoardElements = action.payload.tables;
+            let connections = action.payload.connections;
+
+            //action.payload.drawBoardContent
+            if(Array.isArray(action.payload.elements) )
+            {
+                state = [];
+                drawBoardElements.forEach( element => state.elements.push(element))
+            }
+
+            if(Array.isArray(action.payload.connections) )
+            {
+                connections = connections.map(connection => {
+                    return {...connection, withLabel: false, horizontalAlignment: true, withArrow: true, isSelected: false}
+                });
+                state = [];
+                connections.forEach(connection => state.connections.push(connection))
+            }
+            return state;
+        },
+        /**
+         *
+         * @param state
+         * @param action
+         */
+        UpdateDrawBoardElementSize: (state, action) => {
+            let id = action.payload.id;
+            let width = action.payload.width;
+            let height = action.payload.height;
+
+            let element = state.find(element => element.id === id);
+            element.width = width;
+            element.height = height;
+
+            return state;
+        },
+        /**
+         *
+         * @param state
+         * @param action
+         */
+        UpdateDrawBoardElementPosition: (state, action) => {
+            let id = action.payload.id;
+            let x = action.payload.x;
+            let y = action.payload.y;
+
+            let element = state.find(element => element.id === id);
+            element.x = x;
+            element.y = y;
+
+            return state;
+        }
+    }
+})
+
+export const {
+    ImportContent,
+    UpdateDrawBoardElementSize,
+    UpdateDrawBoardElementPosition
+    } = relationalContentSlice.actions;
+
+export default relationalContentSlice.reducer;
+export const selectRelationalContentSlice = (state) => state.relationalContent;
